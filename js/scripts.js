75b82ee1b138c20d26fdba4a2986971ff1438325
Robert Lu <robertlu99@yahoo.com>
2017-01-11 02:32:48 -0800

    Working on PHP submission

--- js/scripts.js
+++ js/scripts.js
@@ -59,5 +59,20 @@ $(document).ready(function () {
         $('.email-div').find('.vertical-line').css('background', '#454545');
         $('.email-div').css('border', '3px solid #454545');
     });
+    $('.send').click(function () {
+        var name = $.trim($('.name-field').val()), email = $.trim($('.email-field').val()), message = $.trim($('.message-field').val()), pattern = /^([a-z\d!#$%&'*+\-\/=?\^_`{|}~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]+(\.[a-z\d!#$%&'*+\-\/=?\^_`{|}~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]+)*|"((([ \t]*\r\n)?[ \t]+)?([\x01-\x08\x0b\x0c\x0e-\x1f\x7f\x21\x23-\x5b\x5d-\x7e\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]|\\[\x01-\x09\x0b\x0c\x0d-\x7f\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]))*(([ \t]*\r\n)?[ \t]+)?")@(([a-z\d\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]|[a-z\d\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF][a-z\d\-._~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]*[a-z\d\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])\.)+([a-z\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]|[a-z\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF][a-z\d\-._~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]*[a-z\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])\.?$/i;
+        if (name.length <= 0) {
+            $('.name-div').css('border', '3px solid #FF0000');
+            return;
+        }
+        if (email.length <= 0 || !email.match(pattern)) {
+            $('.email-div').css('border', '3px solid #FF0000');
+            return;
+        }
+        if (message.length <= 2) {
+            $('.message-field').css('border', '3px solid #FF0000');
+            return;
+        }
+    });
 });
 
